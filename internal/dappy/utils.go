de79bfa02eab35431f28cd851fc029429fa246cf
Pinghao Wu <xdavidwuph@gmail.com>
2024-02-09 00:51:14 +0800

    dappy: drop pre-cgi exit status func

--- internal/dappy/utils.go
+++ internal/dappy/utils.go
@@ -1,12 +1,1 @@
 package dappy
-
-func ExitCodeToHttpStatus(c int) int {
-	switch {
-	case c == 0:
-		return 200
-	case c > 128: // killed by signal
-		return 500
-	default:
-		return c + 399
-	}
-}
