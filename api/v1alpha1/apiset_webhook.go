b40acdaf28ec5b5d87076b78bb9c3b0f613f7a97
Pinghao Wu <xdavidwuph@gmail.com>
2024-04-14 21:11:24 +0800

    webhook: validate json schema itself
    impl of webhooks make our api package fat, but let's keep it align with
    kubebuilder scaffolding for now, wait and see how they are going to
    seperate it

--- api/v1alpha1/apiset_webhook.go
+++ api/v1alpha1/apiset_webhook.go
@@ -1,7 +1,11 @@
 package v1alpha1
 
 import (
+	"github.com/santhosh-tekuri/jsonschema/v5"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -17,9 +21,6 @@ func (r *APISet) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 //+kubebuilder:webhook:path=/validate-fluorescence-aic-cs-nycu-edu-tw-v1alpha1-apiset,mutating=false,failurePolicy=fail,sideEffects=None,groups=fluorescence.aic.cs.nycu.edu.tw,resources=apisets,verbs=create;update,versions=v1alpha1,name=vapiset.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &APISet{}
@@ -28,22 +29,43 @@ var _ webhook.Validator = &APISet{}
 func (r *APISet) ValidateCreate() (admission.Warnings, error) {
 	apisetlog.Info("validate create", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil, nil
+	return r.validate()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *APISet) ValidateUpdate(old runtime.Object) (admission.Warnings, error) {
 	apisetlog.Info("validate update", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object update.
-	return nil, nil
+	return r.validate()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *APISet) ValidateDelete() (admission.Warnings, error) {
 	apisetlog.Info("validate delete", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object deletion.
+	return nil, nil
+}
+
+func (r APISet) validate() (admission.Warnings, error) {
+	errs := []*field.Error{}
+	for i, api := range r.Spec.APIs {
+		path := field.NewPath("spec", "apis")
+		if api.Request != nil && api.Request.Schema != nil {
+			_, err := jsonschema.CompileString("api.schema.json", api.Request.Schema.RawJSON)
+			if err != nil {
+				errs = append(errs, field.Invalid(
+					path.Index(i).Child("request", "schema"),
+					api.Request.Schema.RawJSON,
+					err.(*jsonschema.SchemaError).Unwrap().Error(),
+				))
+			}
+		}
+	}
+
+	if len(errs) != 0 {
+		return nil, errors.NewInvalid(
+			schema.GroupKind{Group: GroupVersion.Group, Kind: "APTSet"},
+			r.Name, errs)
+	}
 	return nil, nil
 }
