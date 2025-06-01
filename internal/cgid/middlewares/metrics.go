33b1c8faee926bb247f2a14c3098a1c48333d228
Pinghao Wu <xdavidwuph@gmail.com>
2025-06-01 10:57:05 +0800

    cgid: middlewares: separate prometheus stuff

---
+++ internal/cgid/middlewares/metrics.go
@@ -0,0 +1,55 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	httpRequests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Number of the http requests received since the server started",
+		},
+		[]string{"handler", "code"},
+	)
+	httpRequestsDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "http_requests_duration_seconds",
+			Help: "Duration in seconds to serve http requests",
+			// TODO consider buckets
+		},
+		[]string{"handler", "code"},
+	)
+	httpInflightRequests = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "http_inflight_requests",
+			Help: "Number of the inflight http requests",
+		},
+		[]string{"handler"},
+	)
+)
+
+func MustRegisterCollectors(r *prometheus.Registry) {
+	r.MustRegister(httpRequests, httpRequestsDuration, httpInflightRequests)
+}
+
+func Intrument(next http.Handler, name string) http.Handler {
+	prepopulateLabels := prometheus.Labels{"handler": name, "code": "200"}
+	httpRequests.With(prepopulateLabels)
+	httpRequestsDuration.With(prepopulateLabels)
+
+	labels := prometheus.Labels{"handler": name}
+	return promhttp.InstrumentHandlerCounter(
+		httpRequests.MustCurryWith(labels),
+		promhttp.InstrumentHandlerDuration(
+			httpRequestsDuration.MustCurryWith(labels),
+			promhttp.InstrumentHandlerInFlight(
+				httpInflightRequests.With(labels),
+				next,
+			),
+		),
+	)
+}
--- internal/cgid/middlewares/middlewares.go
+++ internal/cgid/middlewares/middlewares.go
@@ -6,42 +6,11 @@ import (
 	"net/http"
 
 	"github.com/go-logr/logr"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	jsonschema "github.com/santhosh-tekuri/jsonschema/v5"
 	"k8s.io/apimachinery/pkg/util/rand"
 
 	"github.com/xdavidwu/kube-cgi/internal/cgid"
 )
-
-var (
-	httpRequests = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "http_requests_total",
-			Help: "Number of the http requests received since the server started",
-		},
-		[]string{"handler", "code"},
-	)
-	httpRequestsDuration = prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name: "http_requests_duration_seconds",
-			Help: "Duration in seconds to serve http requests",
-			// TODO consider buckets
-		},
-		[]string{"handler", "code"},
-	)
-	httpInflightRequests = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "http_inflight_requests",
-			Help: "Number of the inflight http requests",
-		},
-		[]string{"handler"},
-	)
-)
-
-func MustRegisterCollectors(r *prometheus.Registry) {
-	r.MustRegister(httpRequests, httpRequestsDuration, httpInflightRequests)
-}
 
 func DrainBody(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -106,22 +75,4 @@ func LogWithIdentifier(next http.Handler) http.Handler {
 		log.Info("requested", "method", r.Method, "uri", r.RequestURI, "referer", r.Referer(), "agent", r.UserAgent())
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
-}
-
-func Intrument(next http.Handler, name string) http.Handler {
-	prepopulateLabels := prometheus.Labels{"handler": name, "code": "200"}
-	httpRequests.With(prepopulateLabels)
-	httpRequestsDuration.With(prepopulateLabels)
-
-	labels := prometheus.Labels{"handler": name}
-	return promhttp.InstrumentHandlerCounter(
-		httpRequests.MustCurryWith(labels),
-		promhttp.InstrumentHandlerDuration(
-			httpRequestsDuration.MustCurryWith(labels),
-			promhttp.InstrumentHandlerInFlight(
-				httpInflightRequests.With(labels),
-				next,
-			),
-		),
-	)
 }
